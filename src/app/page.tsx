76f4c61aa40c398af9b40f7e42a703a0ad00934f
shellagroup2030-droid <shellagroup2030@gmail.com>
2025-09-07 15:33:43 +0300

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
 // src/app/page.tsx
 import HomePage from "@/components/HomePage";
-import { validateRequest } from "@/lib/action";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-	const { user } = await validateRequest();
-
-	if (!user) {
-		redirect("/login");
-	}
-
 	return (
 		<div>
-			<h1 className="flex justify-center text-3xl md:text-4xl">
-				Welcome, M.R {user.username}
-			</h1>
+			<h1 className="flex justify-center text-3xl md:text-4xl"></h1>
 			<HomePage />
 		</div>
 	);
 }
+
