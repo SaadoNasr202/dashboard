19df8215b8f74a3e185762d3b625e39848b014ca
shellagroup2030-droid <shellagroup2030@gmail.com>
2025-09-07 17:21:56 +0300

    de

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,44 @@
 // src/app/page.tsx
-import HomePage from "@/components/HomePage";
+"use client";
 
-export default async function Home() {
+import HomePage from "@/components/HomePage";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+	const router = useRouter();
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		async function checkLoginStatus() {
+			try {
+				const response = await fetch("/api/is_logged_in", {
+					credentials: "include", // ضروري لحتى يبعث الكوكي
+				});
+
+				if (!response.ok) {
+					router.push("/login");
+					return;
+				}
+
+				const data = await response.json();
+
+				if (data.isLoggedIn) {
+					setIsLoading(false);
+				} else {
+					router.push("/login");
+				}
+			} catch (error) {
+				console.error("Login check failed:", error);
+				router.push("/login");
+			}
+		}
+
+		checkLoginStatus();
+	}, [router]);
+
+	if (isLoading) return <div>Loading...</div>;
+
 	return (
 		<div>
 			<h1 className="flex justify-center text-3xl md:text-4xl"></h1>
@@ -9,4 +46,3 @@ export default async function Home() {
 		</div>
 	);
 }
-
